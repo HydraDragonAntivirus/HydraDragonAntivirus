2ebf05ac9644aed6cc40fabd4ce1df73f02348ad
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-05-28 19:01:30 +0300

    fix system_platform and checkpoint

--- antivirus.py
+++ antivirus.py
@@ -268,21 +268,20 @@ def load_data():
 
 def hasMicrosoftSignature(path):
     try:
-        system = platform.system()
-        if system == "Windows":
+        if system_platform == "Windows":
             command = f"Get-AuthenticodeSignature '{path}' | Format-List"
             result = subprocess.run(["powershell.exe", "-Command", command], capture_output=True, text=True)
             if "O=Microsoft Corporation" in result.stdout:
                 return True
             else:
                 return False
-        elif system == "Darwin":  # macOS
+        elif system_platform == "Darwin":  # macOS
             result = subprocess.run(["codesign", "-dvv", path], capture_output=True, text=True)
             if "Authority=Microsoft Corporation" in result.stdout:
                 return True
             else:
                 return False
-        elif system == "Linux":
+        elif system_platform == "Linux":
             result = subprocess.run(["osslsigncode", "verify", "-in", path], capture_output=True, text=True)
             if "Microsoft Corporation" in result.stdout:
                 return True
@@ -295,14 +294,12 @@ def hasMicrosoftSignature(path):
         print(f"An error occurred: {e}")
         return False
 
-def valid_signature_exists(file_path):
-    system = platform.system()
-    
-    if system == "Windows":
+def valid_signature_exists(file_path):    
+    if system_platform == "Windows":
         return check_windows_signature(file_path)
-    elif system == "Darwin":  # macOS
+    elif system_platform == "Darwin":  # macOS
         return check_macos_signature(file_path)
-    elif system == "Linux":
+    elif system_platform == "Linux":
         return check_linux_signature(file_path)
     else:
         logging.error(f"Unsupported platform: {system}")
