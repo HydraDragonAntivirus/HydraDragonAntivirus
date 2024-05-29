69460c01b653233ba46b007f6aa0086ea321a589
HydraDragonAntivirus <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-05-29 22:06:32 +0300

    Snort fix

--- antivirus.py
+++ antivirus.py
@@ -1016,11 +1016,9 @@ class SnortObserver:
                 if self.snort_process.returncode != 0:
                     logging.info(f"Device number {device_number} is invalid or another error occurred.")
                     print(f"Device number {device_number} is invalid or another error occurred.")
-                    continue
                 else:
                     logging.info(f"Snort started on device number {device_number}.")
                     print(f"Snort started on device number {device_number}.")
-                    break
             except Exception as e:
                 logging.error(f"Failed to start Snort on device {device_number}: {e}")
                 print(f"Failed to start Snort on device {device_number}: {e}")
