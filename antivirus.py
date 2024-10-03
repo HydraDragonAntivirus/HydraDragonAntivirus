a5c2363b270fe41f8d9d35041080eec7d64191d9
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-10-03 13:45:31 +0300

    make him debug due to too many messages

--- antivirus.py
+++ antivirus.py
@@ -1052,7 +1052,7 @@ class RealTimeWebProtectionHandler:
                 self.scan_ip_address(packet[IP].src)
                 self.scan_ip_address(packet[IP].dst)
             else:
-                logging.info("IP layer or DNS layer not found in the packet.")
+                logging.debug("IP layer or DNS layer not found in the packet.")
                 
         except Exception as e:
             logging.error(f"Error handling IPv4 packet: {e}")
