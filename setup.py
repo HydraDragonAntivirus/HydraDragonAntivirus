ffca358ef49ef42ec92746776a5007e7105f8448
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2024-11-22 16:05:37 +0300

    Switch to cx_freeze the only good option

---
+++ setup.py
@@ -0,0 +1,28 @@
+from cx_Freeze import setup, Executable
+
+# Define the executable and options
+executables = [
+    Executable(
+        "antivirus.py",  # Your script
+        target_name="antivirus.exe",  # Output executable name
+        base=None,  # 'None' for a GUI application (no console window)
+        icon="assets/HydraDragonAV.ico",  # Path to your .ico file
+        uac_admin=True  # Request admin privileges
+    )
+]
+
+# Fine-tune build options (adjust as needed)
+build_options = {
+    "packages": [],  # List packages if any are required
+    "includes": ["preshed.maps"],  # Include necessary modules
+    "excludes": ["tkinter"],  # Exclude unnecessary modules like tkinter
+}
+
+# Setup configuration for cx_Freeze
+setup(
+    name="HydraDragon Antivirus",  # Application name
+    version="1.0",  # Version number
+    description="HydraDragon Antivirus for Windows - A comprehensive malware analysis tool utilizing dynamic/static analysis, machine learning, and behavior analysis.",
+    options={"build_exe": build_options},  # Build options
+    executables=executables,  # List of executables
+)
