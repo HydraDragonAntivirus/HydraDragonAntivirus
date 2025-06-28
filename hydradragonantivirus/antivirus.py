3aa0e4d865b43e4c705040894d364922e0de4a21
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2025-06-28 23:26:29 +0300

    Solving the Advanced Installer

--- hydradragonantivirus/antivirus.py
+++ hydradragonantivirus/antivirus.py
@@ -990,120 +990,205 @@ def get_unique_output_path(output_dir: Path, base_name) -> Path:
 # https://gist.github.com/KasparNagu/9ee02cb62d81d9e4c7a833518a710d6e
 
 class AdvancedInstallerFileInfo:
-        def __init__(self, name, size, offset, xorSize):
-                self.name = name
-                self.size = size
-                self.offset = offset
-                self.xorSize = xorSize
-        def __repr__(self):
-                return "[%s size=%d offset=%d]" % (self.name, self.size, self.offset)
+    def __init__(self, name, size, offset, xorSize):
+        self.name = name
+        self.size = size
+        self.offset = offset
+        self.xorSize = xorSize
+
+    def __repr__(self):
+        return "[%s size=%d offset=%d]" % (self.name, self.size, self.offset)
+
 
 class AdvancedInstallerFileReader:
-        def __init__(self,filehandle,size,keepOpen,xorLength):
-                self.filehandle = filehandle
-                self.size = size
-                self.xorLength = xorLength
-                self.pos = 0
-                self.keepOpen = keepOpen
-        def xorFF(self,block):
-                if isinstance(block,str):
-                         return "".join([chr(ord(i)^0xff) for i in block])
-                else:
-                         return bytes([i^0xff for i in block])
-        def read(self,size = None):
-                if size is None:
-                        return self.read(self.size - self.pos)
-                if self.pos < self.xorLength:
-                        xorLen = min(self.xorLength - self.pos, size)
-                        xorBlock = self.filehandle.read(xorLen)
-                        xorLenEffective = len(xorBlock)
-                        self.pos += xorLenEffective
-                        xorBlock = self.xorFF(xorBlock)
-                        if xorLenEffective < size:
-                                return xorBlock + self.read(size - xorLenEffective)
-                        return xorBlock
-                blk = self.filehandle.read(min(size,self.size - self.pos))
-                self.pos += len(blk)
-                return blk
-        def close(self):
-                if not self.keepOpen:
-                        self.filehandle.close()
-        def __enter__(self):
-                return self
-        def __exit__(self, type, value, traceback):
-                self.close()
+    def __init__(self, filehandle, size, keepOpen, xorLength):
+        self.filehandle = filehandle
+        self.size = size
+        self.xorLength = xorLength
+        self.pos = 0
+        self.keepOpen = keepOpen
+
+    def xorFF(self, block):
+        if isinstance(block, str):
+            return "".join([chr(ord(i) ^ 0xff) for i in block])
+        else:
+            return bytes([i ^ 0xff for i in block])
+
+    def read(self, size=None):
+        if size is None:
+            return self.read(self.size - self.pos)
+        if self.pos < self.xorLength:
+            xorLen = min(self.xorLength - self.pos, size)
+            xorBlock = self.filehandle.read(xorLen)
+            xorLenEffective = len(xorBlock)
+            self.pos += xorLenEffective
+            xorBlock = self.xorFF(xorBlock)
+            if xorLenEffective < size:
+                return xorBlock + self.read(size - xorLenEffective)
+            return xorBlock
+        blk = self.filehandle.read(min(size, self.size - self.pos))
+        self.pos += len(blk)
+        return blk
+
+    def close(self):
+        if not self.keepOpen:
+            self.filehandle.close()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
 
 class AdvancedInstallerReader:
-        def __init__(self,filename,debug=None):
-                self.filename = filename
-                self.filehandle = open(filename,"rb")
-                self.search_back = 10000
-                self.xorSize = 0x200
-                self.footer_position = None
-                self.debug = debug
-                self.threadsafeReopen = False
-                self.files = []
-        def close(self):
-                self.filehandle.close()
-        def search_footer(self):
-                for i in range(0,10000):
-                        self.filehandle.seek(-i,os.SEEK_END) 
-                        magic = self.filehandle.read(10)
-                        if magic == b"ADVINSTSFX":
-                                self.footer_position = i + 0x48 - 12
-                                break
-                if self.footer_position is None:
-                        logging.error("ADVINSTSFX not found")
-        def read_footer(self):
-                if self.footer_position is None:
-                        self.search_footer()
-                self.filehandle.seek(-self.footer_position,os.SEEK_END)
-                footer = self.filehandle.read(0x48)
-                offset, self.nfiles, dummy1, offset1, self.info_off, file_off, hexhash, dummy2, name, = struct.unpack("<llllll32sl12s", footer)
+    def __init__(self, filename, debug=None):
+        self.filename = filename
+        self.filehandle = open(filename, "rb")
+        self.search_back = 10000
+        self.xorSize = 0x200
+        self.footer_position = None
+        self.debug = debug
+        self.threadsafeReopen = False
+        self.files = []
+
+    def close(self):
+        self.filehandle.close()
+
+    def search_footer(self):
+        for i in range(0, 10000):
+            self.filehandle.seek(-i, os.SEEK_END)
+            magic = self.filehandle.read(10)
+            if magic == b"ADVINSTSFX":
+                self.footer_position = i + 0x48 - 12
+                break
+        if self.footer_position is None:
+            raise Exception("ADVINSTSFX not found")
+
+    def read_footer(self):
+        if self.footer_position is None:
+            self.search_footer()
+        self.filehandle.seek(-self.footer_position, os.SEEK_END)
+        footer = self.filehandle.read(0x48)
+
+        if self.debug:
+            self.debug.write("Footer data (%d bytes): %s\n" % (len(footer), footer.hex()))
+
+        # Try different unpacking strategies based on actual footer structure
+        try:
+            # Original format - try first
+            offset, self.nfiles, dummy1, offset1, self.info_off, file_off, hexhash, dummy2, name = struct.unpack(
+                "<llllll32sl12s", footer)
+        except struct.error:
+            try:
+                # Alternative format without the last name field
+                data = struct.unpack("<llllll32sl", footer[:60])
+                offset, self.nfiles, dummy1, offset1, self.info_off, file_off, hexhash, dummy2 = data
+                name = footer[60:] if len(footer) > 60 else b""
+            except struct.error:
+                try:
+                    # Simplified format - just the essential fields
+                    data = struct.unpack("<llllll", footer[:24])
+                    offset, self.nfiles, dummy1, offset1, self.info_off, file_off = data
+                    hexhash = footer[24:56] if len(footer) > 56 else b""
+                    dummy2 = 0
+                    name = footer[56:] if len(footer) > 56 else b""
+                except struct.error:
+                    # Last resort - extract what we can
+                    if len(footer) >= 8:
+                        offset, self.nfiles = struct.unpack("<ll", footer[:8])
+                        dummy1 = offset1 = 0
+                        self.info_off = struct.unpack("<l", footer[16:20])[0] if len(footer) >= 20 else 0
+                        file_off = struct.unpack("<l", footer[20:24])[0] if len(footer) >= 24 else 0
+                    else:
+                        raise Exception("Footer too short to parse")
+                    hexhash = b""
+                    dummy2 = 0
+                    name = b""
+
+        if self.debug:
+            self.debug.write(
+                "offset=%d files=%d offset1=%d  info_off=%d file_off=%d hexhash=%s name=%s\n" % (offset, self.nfiles,
+                                                                                                 offset1, self.info_off,
+                                                                                                 file_off, hexhash,
+                                                                                                 name))
+
+    def read_info(self):
+        self.read_footer()
+        self.files = []
+        self.filehandle.seek(self.info_off, os.SEEK_SET)
+        for i in range(0, self.nfiles):
+            info = self.filehandle.read(24)
+            if len(info) < 24:
                 if self.debug:
-                        self.debug.write("offset=%d files=%d offset1=%d  info_off=%d file_off=%d hexhash=%s name=%s\n" % (offset,self.nfiles,offset1,self.info_off,file_off,hexhash,name))
-        def read_info(self):
-                self.read_footer()
-                self.files = []
-                self.filehandle.seek(self.info_off,os.SEEK_SET)
-                for i in range(0,self.nfiles):
-                        info = self.filehandle.read(24)
-                        dummy1, dummy2, dummy3, size, offset, namesize = struct.unpack("<llllll",info)
-                        if self.debug:
-                                self.debug.write(" size=%d offset=%d namesize=%d dummy1=0x%x dummy2=0x%x dummy3=0x%x\n" % (size,offset,namesize,dummy1,dummy2,dummy3))
-                        if namesize < 0xFFFF:
-                                name = self.filehandle.read(namesize*2)
-                                name = name.decode("UTF-16")
-                                if self.debug:
-                                        self.debug.write("  name=%s\n" % name)
-                                self.files.append(AdvancedInstallerFileInfo(name,size,offset,self.xorSize if dummy3 == 2 else 0))
-                        else:
-                                logging.error("Invalid name size %d" % namesize)
-        def open(self,infoFile):
-                if isinstance(infoFile,AdvancedInstallerFileInfo):
-                        if self.threadsafeReopen:
-                                fh = open(self.filename,"rb")
-                        else:
-                                fh = self.filehandle
-                        fh.seek(infoFile.offset,os.SEEK_SET)
-                        return AdvancedInstallerFileReader(fh,infoFile.size,not self.threadsafeReopen,infoFile.xorSize)
+                    self.debug.write("Warning: incomplete info block for file %d\n" % i)
+                break
+            dummy1, dummy2, dummy3, size, offset, namesize = struct.unpack("<llllll", info)
+            if self.debug:
+                self.debug.write(
+                    " size=%d offset=%d namesize=%d dummy1=0x%x dummy2=0x%x dummy3=0x%x\n" % (size, offset, namesize,
+                                                                                              dummy1, dummy2, dummy3))
+            if 0 < namesize < 0xFFFF:
+                name_data = self.filehandle.read(namesize * 2)
+                if len(name_data) == namesize * 2:
+                    try:
+                        name = name_data.decode("UTF-16LE")
+                        # Remove null terminator if present
+                        name = name.rstrip('\x00')
+                    except UnicodeDecodeError:
+                        # Fallback to UTF-16BE or raw bytes
+                        try:
+                            name = name_data.decode("UTF-16BE")
+                            name = name.rstrip('\x00')
+                        except UnicodeDecodeError:
+                            name = "file_%d.bin" % i
+                    if self.debug:
+                        self.debug.write("  name=%s\n" % name)
+                    self.files.append(AdvancedInstallerFileInfo(name, size, offset, self.xorSize if dummy3 == 2 else 0))
                 else:
-                        if not self.files:
-                                self.read_info()
-                        for f in files:
-                                if f.name == infoFile:
-                                        return self.open(f)
-                return None
-        def infolist(self):
-                if not self.files:
-                        self.read_info()
-                return self.files
-        def __enter__(self):
-                return self
-        def __exit__(self, type, value, traceback):
-                self.close()
-        def __repr__(self):
-                return "[path=%s footer=%s nFiles=%d]" % (self.filename,self.footer_position,len(self.files))
+                    if self.debug:
+                        self.debug.write("Warning: incomplete name data for file %d\n" % i)
+            elif namesize == 0:
+                # Handle files with no name
+                name = "unnamed_file_%d.bin" % i
+                if self.debug:
+                    self.debug.write("  name=%s (unnamed)\n" % name)
+                self.files.append(AdvancedInstallerFileInfo(name, size, offset, self.xorSize if dummy3 == 2 else 0))
+            else:
+                if self.debug:
+                    self.debug.write("Warning: Invalid name size %d for file %d\n" % (namesize, i))
+                # Skip this file or use a default name
+                continue
+
+    def open(self, infoFile):
+        if isinstance(infoFile, AdvancedInstallerFileInfo):
+            if self.threadsafeReopen:
+                fh = open(self.filename, "rb")
+            else:
+                fh = self.filehandle
+            fh.seek(infoFile.offset, os.SEEK_SET)
+            return AdvancedInstallerFileReader(fh, infoFile.size, not self.threadsafeReopen, infoFile.xorSize)
+        else:
+            if not self.files:
+                self.read_info()
+            for f in self.files:  # Fixed: was 'files' should be 'self.files'
+                if f.name == infoFile:
+                    return self.open(f)
+        return None
+
+    def infolist(self):
+        if not self.files:
+            self.read_info()
+        return self.files
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.close()
+
+    def __repr__(self):
+        return "[path=%s footer=%s nFiles=%d]" % (self.filename, self.footer_position, len(self.files))
 
 def advanced_installer_extractor(file_path):
         """
@@ -7682,7 +7767,7 @@ def extract_all_files_with_7z(file_path, nsis_flag=False):
         # Prepare a unique output directory
         counter = 1
         base_name = os.path.splitext(os.path.basename(file_path))[0]
-        base_output_dir = os.path.join(general_extracted_dir, base_name)
+        base_output_dir = os.path.join(general_extracted_with_7z_dir, base_name)
         while os.path.exists(f"{base_output_dir}_{counter}"):
             counter += 1
         output_dir = f"{base_output_dir}_{counter}"
@@ -10722,7 +10807,7 @@ class AntivirusApp(QWidget):
             if os.path.exists(icon_path):
                  self.setWindowIcon(QIcon(icon_path))
         except Exception as e:
-            print(f"Could not load icon: {e}")
+            logging.error(f"Could not load icon: {e}")
 
         self._set_window_background()
 
