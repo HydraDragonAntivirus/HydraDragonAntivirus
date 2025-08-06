753c8b8e78eee0f3cb382204a51a6a38ea948d35
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2025-08-06 22:26:37 +0300

    Update post_install.bat

--- post_install.bat
+++ post_install.bat
@@ -1,16 +1,21 @@
 @echo off
-REM Install driver INF
+:: Check if running as Administrator
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [!] This script must be run as Administrator.
+    echo [!] Restarting as Administrator...
+    powershell -Command "Start-Process '%~f0' -Verb runAs"
+    exit /b
+)
+echo [+] Installing driver INF...
 RUNDLL32.EXE SETUPAPI.DLL,InstallHinfSection DefaultInstall 132 "%~dp0hydradragon\Owlyshield\OwlyshieldRansomFilter\OwlyshieldRansomFilter.inf"
-
-REM Create and configure service
+echo [+] Creating 'Owlyshield Service'...
 sc create "Owlyshield Service" binPath= "\"%~dp0hydradragon\Owlyshield\Owlyshield Service\owlyshield_ransom.exe\""
+echo [+] Setting service dependency: OwlyshieldRansomFilter
 sc config "Owlyshield Service" depend= OwlyshieldRansomFilter
+echo [+] Setting service start mode: demand
 sc config "Owlyshield Service" start= demand
-
-REM Optionally start the service
-sc start "Owlyshield Service"
-
+echo Done.
 pause
-
-REM Delete this batch file after running
+:: Delete the script after running
 del "%~f0"
