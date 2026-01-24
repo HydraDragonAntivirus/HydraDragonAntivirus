ec4a3a2899b466118720550801b6e7ed06268c2b
HydraDragonAntivirus <130374789+Siradankullanici@users.noreply.github.com>
2026-01-24 19:13:03 +0300

    not a good fix

--- Owlyshield/owlyshield_predict/src/windows/run.rs
+++ Owlyshield/owlyshield_predict/src/windows/run.rs
@@ -244,7 +244,9 @@ pub fn run() {
                     // No sleep here - immediate loop to keep latency down
                 }
             } else {
-                panic!("Can't receive Driver Message?");
+                // If get_irp returns None (e.g. malformed data), just log and continue
+                // DON'T PANIC!
+                // Logging::warning("Received invalid driver message or empty poll"); 
             }
         }
     }
