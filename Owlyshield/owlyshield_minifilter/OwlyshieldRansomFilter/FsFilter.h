65e486dd12253894970f95a98266daced77c2bf0
HydraDragonAntivirus <130374789+Siradankullanici@users.noreply.github.com>
2026-02-06 17:16:58 +0300

    Update FsFilter.h

--- Owlyshield/owlyshield_minifilter/OwlyshieldRansomFilter/FsFilter.h
+++ Owlyshield/owlyshield_minifilter/OwlyshieldRansomFilter/FsFilter.h
@@ -53,7 +53,7 @@ VOID FSInstanceTeardownComplete(_In_ PCFLT_RELATED_OBJECTS FltObjects, _In_ FLT_
 
 // handles pre operation for read, write, set info and close files
 NTSTATUS
-FSProcessPreOperartion(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
+FSProcessPreOperation(_Inout_ PFLT_CALLBACK_DATA Data, _In_ PCFLT_RELATED_OBJECTS FltObjects,
                        _Flt_CompletionContext_Outptr_ PVOID *CompletionContext);
 
 NTSTATUS
