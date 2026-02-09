61f31f0ac2e3d38afcb3f64e798cabaaa32f93be
HydraDragonAntivirus <130374789+Siradankullanici@users.noreply.github.com>
2026-02-09 22:45:45 +0300

    Update UserModeHookEngine.cpp

--- Owlyshield/owlyshield_minifilter/OwlyshieldRansomFilter/UserModeHookEngine.cpp
+++ Owlyshield/owlyshield_minifilter/OwlyshieldRansomFilter/UserModeHookEngine.cpp
@@ -33,6 +33,7 @@ typedef NTSTATUS(NTAPI *PZW_ALLOCATE_VIRTUAL_MEMORY)(_In_ HANDLE ProcessHandle, 
                                                      _In_ ULONG_PTR ZeroBits, _Inout_ PSIZE_T RegionSize,
                                                      _In_ ULONG AllocationType, _In_ ULONG Protect);
 typedef NTSTATUS(NTAPI *PZW_DUPLICATE_OBJECT)(_In_ HANDLE SourceProcessHandle, _In_ HANDLE SourceHandle,
+                                              _In_ HANDLE TargetProcessHandle, _Out_ PHANDLE TargetHandle,
                                               _In_ ACCESS_MASK DesiredAccess, _In_ ULONG HandleAttributes,
                                               _In_ ULONG Options);
 typedef NTSTATUS(NTAPI *PZW_FREE_VIRTUAL_MEMORY)(_In_ HANDLE ProcessHandle, _Inout_ PVOID *BaseAddress,
