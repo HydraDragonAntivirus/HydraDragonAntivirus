43b3a308cbfd483f0b3bc528816c59ef88e399fc
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2026-04-08 01:25:14 +0300

    specific build

--- openedr/edrav2/eprj/openssl/build.cmd
+++ openedr/edrav2/eprj/openssl/build.cmd
@@ -1,85 +1,91 @@
 @echo off
+setlocal EnableExtensions
 
 set configure=threads no-shared no-asm no-engine no-tests
 set "root_dir=%~dp0"
 set "root_dir=%root_dir:~0,-1%"
 set "work_dir=%root_dir%\openssl"
 
-pushd "%work_dir%"
+call :normalize_path
+call :resolve_perl
+if errorlevel 1 exit /b 1
 
-call :buildx64-debug
-call :buildx64-release
-call :buildx86-debug
-call :buildx86-release
+pushd "%work_dir%"
+if errorlevel 1 exit /b 1
+
+call :build x64 Debug VC-WIN64A "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" "%root_dir%\build-x64\Debug" "%root_dir%\lib\win-Debug-x64" --debug
+if errorlevel 1 goto :fail
+call :build x64 Release VC-WIN64A "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat" "%root_dir%\build-x64\Release" "%root_dir%\lib\win-Release-x64"
+if errorlevel 1 goto :fail
+call :build x86 Debug VC-WIN32 "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat" "%root_dir%\build-x86\Debug" "%root_dir%\lib\win-Debug-Win32" --debug
+if errorlevel 1 goto :fail
+call :build x86 Release VC-WIN32 "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat" "%root_dir%\build-x86\Release" "%root_dir%\lib\win-Release-Win32"
+if errorlevel 1 goto :fail
 
 popd
-
-exit /b %errorlevel%
- 
-:buildx64-debug
-
-setlocal
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-set "prefix=%root_dir%\build-x64\Debug"
-set "openssl_dir=%prefix%\ssl"
-nmake clean
-perl Configure VC-WIN64A %configure% --debug --prefix="%prefix%" --openssldir="%openssl_dir%"
-nmake install_sw
-
-xcopy "%prefix%\lib\libcrypto.lib" "%root_dir%\lib\win-Debug-x64\" /I /Y
-xcopy "%prefix%\lib\libssl.lib" "%root_dir%\lib\win-Debug-x64\" /I /Y
-xcopy "%prefix%\lib\ossl_static.pdb" "%root_dir%\lib\win-Debug-x64\" /I /Y
-endlocal
 exit /b 0
 
-:buildx64-release
+:fail
+set "BuildError=%errorlevel%"
+popd
+exit /b %BuildError%
 
-setlocal
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-set "prefix=%root_dir%\build-x64\Release"
-set "openssl_dir=%prefix%\ssl"
-nmake clean
-perl Configure VC-WIN64A %configure% --prefix="%prefix%" --openssldir="%openssl_dir%"
-nmake install_sw
-
-xcopy "%prefix%\lib\libcrypto.lib" "%root_dir%\lib\win-Release-x64\" /I /Y
-xcopy "%prefix%\lib\libssl.lib" "%root_dir%\lib\win-Release-x64\" /I /Y
-xcopy "%prefix%\lib\ossl_static.pdb" "%root_dir%\lib\win-Release-x64\" /I /Y
-endlocal
+:normalize_path
+set "NormalizedPath=%PATH%"
+set "Path="
+set "PATH=%NormalizedPath%"
 exit /b 0
 
-:buildx86-debug
-
-setlocal
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
-
-set "prefix=%root_dir%\build-x86\Debug"
-set "openssl_dir=%prefix%\ssl"
-nmake clean
-perl Configure VC-WIN32 %configure% --debug --prefix="%prefix%" --openssldir="%openssl_dir%"
-nmake install_sw
-
-xcopy "%prefix%\lib\libcrypto.lib" "%root_dir%\lib\win-Debug-Win32\" /I /Y
-xcopy "%prefix%\lib\libssl.lib" "%root_dir%\lib\win-Debug-Win32\" /I /Y
-xcopy "%prefix%\lib\ossl_static.pdb" "%root_dir%\lib\win-Debug-Win32\" /I /Y
-endlocal
+:resolve_perl
+set "PerlExe="
+where perl.exe >nul 2>&1
+if not errorlevel 1 set "PerlExe=perl.exe"
+if not defined PerlExe if exist "C:\Strawberry\perl\bin\perl.exe" set "PerlExe=C:\Strawberry\perl\bin\perl.exe"
+if not defined PerlExe if exist "C:\Perl64\bin\perl.exe" set "PerlExe=C:\Perl64\bin\perl.exe"
+if not defined PerlExe if exist "C:\Program Files\Git\usr\bin\perl.exe" set "PerlExe=C:\Program Files\Git\usr\bin\perl.exe"
+if not defined PerlExe (
+	echo Unable to locate a usable perl.exe
+	exit /b 1
+)
+echo Using Perl: %PerlExe%
 exit /b 0
 
-:buildx86-release
-
+:build
 setlocal
-call "%ProgramFiles%\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+set "Arch=%~1"
+set "Config=%~2"
+set "Target=%~3"
+set "VcVars=%~4"
+set "Prefix=%~5"
+set "OutLibDir=%~6"
+set "OpenSslDir=%Prefix%\ssl"
 
-set "prefix=%root_dir%\build-x86\Release"
-set "openssl_dir=%prefix%\ssl"
-nmake clean
-perl Configure VC-WIN32 %configure% --prefix="%prefix%" --openssldir="%openssl_dir%"
+echo ==== Building OpenSSL %Arch% %Config% ====
+
+call "%VcVars%" >nul
+if errorlevel 1 goto :build_fail
+call :normalize_path
+
+if exist makefile (
+	nmake clean
+	if errorlevel 1 goto :build_fail
+)
+"%PerlExe%" Configure %Target% %configure% --prefix="%Prefix%" --openssldir="%OpenSslDir%" %7
+if errorlevel 1 goto :build_fail
 nmake install_sw
+if errorlevel 1 goto :build_fail
 
-xcopy "%prefix%\lib\libcrypto.lib" "%root_dir%\lib\win-Release-Win32\" /I /Y
-xcopy "%prefix%\lib\libssl.lib" "%root_dir%\lib\win-Release-Win32\" /I /Y
-xcopy "%prefix%\lib\ossl_static.pdb" "%root_dir%\lib\win-Release-Win32\" /I /Y
-endlocal
-exit /b 0
+xcopy "%Prefix%\lib\libcrypto.lib" "%OutLibDir%\" /I /Y >nul
+if errorlevel 1 goto :build_fail
+if not exist "%OutLibDir%\libcrypto.lib" goto :build_fail
+
+xcopy "%Prefix%\lib\libssl.lib" "%OutLibDir%\" /I /Y >nul
+if errorlevel 1 goto :build_fail
+if not exist "%OutLibDir%\libssl.lib" goto :build_fail
+
+xcopy "%Prefix%\lib\ossl_static.pdb" "%OutLibDir%\" /I /Y >nul
+
+endlocal & exit /b 0
+
+:build_fail
+endlocal & exit /b 1
---
+++ openedr/edrav2/eprj/openssl/openssl/crypto/asn1/d2i_pu.d
@@ -0,0 +1,73 @@
+d2i_pu.c
+Note: including file: C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\stdio.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt.h
+Note: including file:   C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\vcruntime.h
+Note: including file:    C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\sal.h
+Note: including file:     C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\concurrencysal.h
+Note: including file:    C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\vadefs.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_wstdio.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_stdio_config.h
+Note: including file: C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\internal/cryptlib.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\stdlib.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_malloc.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_search.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\stddef.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_wstdlib.h
+Note: including file:   C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\limits.h
+Note: including file:  C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\string.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_memory.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_memcpy_s.h
+Note: including file:     C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\errno.h
+Note: including file:     C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\vcruntime_string.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_wstring.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/crypto.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\time.h
+Note: including file:    C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\corecrt_wtime.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/e_os2.h
+Note: including file:    C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:     C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslv.h
+Note: including file:    C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\stdint.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/safestack.h
+Note: including file:    C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/stack.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/ossl_typ.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/cryptoerr.h
+Note: including file:    C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/symhacks.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/buffer.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/buffererr.h
+Note: including file:   C:\Program Files (x86)\Windows Kits\10\include\10.0.26100.0\ucrt\sys/types.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/bio.h
+Note: including file:   C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.44.35207\include\stdarg.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/bioerr.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/err.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/lhash.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\internal/nelem.h
+Note: including file: C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/bn.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/bnerr.h
+Note: including file: C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/evp.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/evperr.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/objects.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/obj_mac.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/asn1.h
+Note: including file:    C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:    C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/asn1err.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/objectserr.h
+Note: including file: C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/rsa.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/rsaerr.h
+Note: including file: C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/dsa.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/dh.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/dherr.h
+Note: including file:    C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/dsaerr.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file: C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/ec.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/ecerr.h
+Note: including file:   C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\openssl/opensslconf.h
+Note: including file: C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\crypto\include\internal/evp_int.h
+Note: including file:  C:\Users\semae\OneDrive\Belgeler\GitHub\HydraDragonAntivirus\openedr\edrav2\eprj\openssl\openssl\include\internal/refcount.h
