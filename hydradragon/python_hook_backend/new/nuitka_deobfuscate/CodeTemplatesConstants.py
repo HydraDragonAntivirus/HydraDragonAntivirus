b573299e0ff972e18de0453f0342147b63a47fc6
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2026-04-12 01:33:49 +0300

    update

---
+++ hydradragon/python_hook_backend/new/nuitka_deobfuscate/CodeTemplatesConstants.py
@@ -0,0 +1,296 @@
+#     Copyright 2026, Kay Hayen, mailto:kay.hayen@gmail.com find license text at end of file
+
+
+"""Templates for the constants handling.
+
+spell-checker: ignore structseq
+"""
+
+template_constants_reading = r"""
+#include "nuitka/prelude.h"
+#include <structseq.h>
+
+#include "build_definitions.h"
+
+// Global constants storage
+PyObject *global_constants[%(global_constants_count)d] = {0};
+
+// Sentinel PyObject to be used for all our call iterator endings. It will
+// become a PyCObject pointing to NULL. It's address is unique, and that's
+// enough for us to use it as sentinel value.
+PyObject *Nuitka_sentinel_value = NULL;
+
+PyObject *Nuitka_dunder_compiled_value = NULL;
+
+
+#if _NUITKA_STANDALONE_MODE
+extern PyObject *getStandaloneSysExecutablePath(PyObject *basename);
+
+NUITKA_MAY_BE_UNUSED static PyObject *STRIP_DIRNAME(PyObject *path) {
+#if PYTHON_VERSION < 0x300
+    char const *path_cstr = PyString_AS_STRING(path);
+
+#ifdef _WIN32
+    char const *last_sep = strrchr(path_cstr, '\\');
+#else
+    char const *last_sep = strrchr(path_cstr, '/');
+#endif
+    if (unlikely(last_sep == NULL)) {
+        Py_INCREF(path);
+        return path;
+    }
+
+    return PyString_FromStringAndSize(path_cstr, last_sep - path_cstr);
+#else
+#ifdef _WIN32
+    Py_ssize_t dot_index = PyUnicode_Find(path, const_str_backslash, 0, PyUnicode_GetLength(path), -1);
+#else
+    Py_ssize_t dot_index = PyUnicode_Find(path, const_str_slash, 0, PyUnicode_GetLength(path), -1);
+#endif
+    if (likely(dot_index != -1)) {
+        return PyUnicode_Substring(path, 0, dot_index);
+    } else {
+        Py_INCREF(path);
+        return path;
+    }
+#endif
+}
+#endif
+
+extern void setDistributionsMetadata(PyThreadState *tstate, PyObject *metadata_items);
+
+// We provide the sys.version info shortcut as a global value here for ease of use.
+PyObject *Py_SysVersionInfo = NULL;
+
+#if _NUITKA_MODULE_MODE
+static void _createGlobalConstants(PyThreadState *tstate, PyObject *real_module_name) {
+#else
+static void _createGlobalConstants(PyThreadState *tstate) {
+#endif
+    // We provide the sys.version info shortcut as a global value here for ease of use.
+    Py_SysVersionInfo = Nuitka_SysGetObject("version_info");
+
+    // The empty name means global.
+    loadConstantsBlob(tstate, &global_constants[0], "");
+
+#if _NUITKA_EXE_MODE || _NUITKA_DLL_MODE
+    /* Set the "sys.executable" path to the original CPython executable or point to inside the
+       distribution for standalone. */
+    Nuitka_SysSetObject(
+        "executable",
+#if !_NUITKA_STANDALONE_MODE
+        %(sys_executable)s
+#else
+        getStandaloneSysExecutablePath(%(sys_executable)s)
+#endif
+    );
+
+#if !_NUITKA_STANDALONE_MODE
+    /* Set the "sys.prefix" path to the original one. */
+    Nuitka_SysSetObject(
+        "prefix",
+        %(sys_prefix)s
+    );
+
+    /* Set the "sys.prefix" path to the original one. */
+    Nuitka_SysSetObject(
+        "exec_prefix",
+        %(sys_exec_prefix)s
+    );
+
+
+#if PYTHON_VERSION >= 0x300
+    /* Set the "sys.base_prefix" path to the original one. */
+    Nuitka_SysSetObject(
+        "base_prefix",
+        %(sys_base_prefix)s
+    );
+
+    /* Set the "sys.exec_base_prefix" path to the original one. */
+    Nuitka_SysSetObject(
+        "base_exec_prefix",
+        %(sys_base_exec_prefix)s
+    );
+
+#endif
+#endif
+#endif
+
+    static PyTypeObject Nuitka_VersionInfoType;
+
+    // Same fields as "sys.version_info" except no serial number
+    // spell-checker: ignore releaselevel
+    static PyStructSequence_Field Nuitka_VersionInfoFields[] = {
+        {(char *)"major", (char *)"Major release number"},
+        {(char *)"minor", (char *)"Minor release number"},
+        {(char *)"micro", (char *)"Micro release number"},
+        {(char *)"releaselevel", (char *)"'alpha', 'beta', 'candidate', or 'release'"},
+        {(char *)"containing_dir", (char *)"directory of the containing binary"},
+        {(char *)"standalone", (char *)"boolean indicating standalone mode usage"},
+        {(char *)"onefile", (char *)"boolean indicating onefile mode usage"},
+        {(char *)"macos_bundle_mode", (char *)"boolean indicating macOS app bundle mode usage"},
+        {(char *)"no_asserts", (char *)"boolean indicating --python-flag=no_asserts usage"},
+        {(char *)"no_docstrings", (char *)"boolean indicating --python-flag=no_docstrings usage"},
+        {(char *)"no_annotations", (char *)"boolean indicating --python-flag=no_annotations usage"},
+        {(char *)"module", (char *)"boolean indicating --module usage"},
+        {(char *)"main", (char *)"name of main module at runtime"},
+        {(char *)"original_argv0", (char *)"original argv[0] as received by the onefile binary, None otherwise"},
+        {0}
+    };
+
+    static PyStructSequence_Desc Nuitka_VersionInfoDesc = {
+        (char *)"__nuitka_version__",                                       /* name */
+        (char *)"__compiled__\\n\\nVersion information as a named tuple.",  /* doc */
+        Nuitka_VersionInfoFields,                                           /* fields */
+        sizeof(Nuitka_VersionInfoFields) / sizeof(PyStructSequence_Field)-1 /* count */
+    };
+
+    PyStructSequence_InitType(&Nuitka_VersionInfoType, &Nuitka_VersionInfoDesc);
+
+    Nuitka_dunder_compiled_value = PyStructSequence_New(&Nuitka_VersionInfoType);
+    assert(Nuitka_dunder_compiled_value != NULL);
+
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 0, Nuitka_PyInt_FromLong(%(nuitka_version_major)s));
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 1, Nuitka_PyInt_FromLong(%(nuitka_version_minor)s));
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 2, Nuitka_PyInt_FromLong(%(nuitka_version_micro)s));
+
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 3, Nuitka_String_FromString("%(nuitka_version_level)s"));
+
+    PyObject *containing_directory = getContainingDirectoryObject(false);
+#if _NUITKA_STANDALONE_MODE
+#if !_NUITKA_ONEFILE_MODE
+    containing_directory = STRIP_DIRNAME(containing_directory);
+#endif
+
+#if _NUITKA_MACOS_BUNDLE_MODE
+    containing_directory = STRIP_DIRNAME(containing_directory);
+    containing_directory = STRIP_DIRNAME(containing_directory);
+#endif
+#endif
+
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 4, containing_directory);
+
+#if _NUITKA_STANDALONE_MODE
+    PyObject *is_standalone_mode = Py_True;
+#else
+    PyObject *is_standalone_mode = Py_False;
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 5, is_standalone_mode);
+#ifdef _NUITKA_ONEFILE_MODE
+    PyObject *is_onefile_mode = Py_True;
+#else
+    PyObject *is_onefile_mode = Py_False;
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 6, is_onefile_mode);
+
+#if _NUITKA_MACOS_BUNDLE_MODE
+    PyObject *is_macos_bundle_mode = Py_True;
+#else
+    PyObject *is_macos_bundle_mode = Py_False;
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 7, is_macos_bundle_mode);
+
+#if _NUITKA_NO_ASSERTS == 1
+    PyObject *is_no_asserts = Py_True;
+#else
+    PyObject *is_no_asserts = Py_False;
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 8, is_no_asserts);
+
+#if _NUITKA_NO_DOCSTRINGS == 1
+    PyObject *is_no_docstrings = Py_True;
+#else
+    PyObject *is_no_docstrings = Py_False;
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 9, is_no_docstrings);
+
+#if _NUITKA_NO_ANNOTATIONS == 1
+    PyObject *is_no_annotations = Py_True;
+#else
+    PyObject *is_no_annotations = Py_False;
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 10, is_no_annotations);
+
+#if _NUITKA_MODULE_MODE
+    PyObject *is_module_mode = Py_True;
+#else
+    PyObject *is_module_mode = Py_False;
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 11, is_module_mode);
+
+#if _NUITKA_MODULE_MODE
+    PyObject *main_name = real_module_name;
+    Py_INCREF(real_module_name);
+#else
+    PyObject *main_name = Nuitka_String_FromString("__main__");
+#endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 12, main_name);
+
+#if _NUITKA_EXE_MODE || _NUITKA_DLL_MODE
+    PyObject *original_argv0 = getOriginalArgv0Object();
+#else
+    PyObject *original_argv0 = Py_None;
+# endif
+    PyStructSequence_SET_ITEM(Nuitka_dunder_compiled_value, 13, original_argv0);
+
+    // Prevent users from creating the Nuitka version type object.
+    Nuitka_VersionInfoType.tp_init = NULL;
+    Nuitka_VersionInfoType.tp_new = NULL;
+
+    // Register included meta data.
+    setDistributionsMetadata(tstate, %(metadata_values)s);
+}
+
+// In debug mode we can check that the constants were not tampered with in any
+// given moment. We typically do it at program exit, but we can add extra calls
+// for sanity.
+#ifndef __NUITKA_NO_ASSERT__
+void checkGlobalConstants(void) {
+// TODO: Ask constant code to check values.
+
+}
+#endif
+
+#if _NUITKA_MODULE_MODE
+void createGlobalConstants(PyThreadState *tstate, PyObject *real_module_name) {
+#else
+void createGlobalConstants(PyThreadState *tstate) {
+#endif
+    if (Nuitka_sentinel_value == NULL) {
+#if PYTHON_VERSION < 0x300
+        Nuitka_sentinel_value = PyCObject_FromVoidPtr(NULL, NULL);
+#else
+        // The NULL value is not allowed for a capsule, so use something else.
+        Nuitka_sentinel_value = PyCapsule_New((void *)27, "sentinel", NULL);
+#endif
+        assert(Nuitka_sentinel_value);
+
+        Py_SET_REFCNT_IMMORTAL(Nuitka_sentinel_value);
+
+#if _NUITKA_MODULE_MODE
+        _createGlobalConstants(tstate, real_module_name);
+#else
+        _createGlobalConstants(tstate);
+#endif
+    }
+}
+"""
+
+from . import TemplateDebugWrapper  # isort:skip
+
+TemplateDebugWrapper.checkDebug(globals())
+
+#     Part of "Nuitka", an optimizing Python compiler that is compatible and
+#     integrates with CPython, but also works on its own.
+#
+#     Licensed under the GNU Affero General Public License, Version 3 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#        http://www.gnu.org/licenses/agpl.txt
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
---
+++ hydradragon/python_hook_backend/new/nuitka_deobfuscate/CodeTemplatesLoader.py
@@ -0,0 +1,191 @@
+#     Copyright 2026, Kay Hayen, mailto:kay.hayen@gmail.com find license text at end of file
+
+
+"""Templates for the loading of embedded modules."""
+
+template_metapath_loader_compiled_module_entry = """\
+{%(module_name)s, module_code_%(module_identifier)s, 0, 0, %(flags)s
+#if defined(_NUITKA_FREEZER_HAS_FILE_PATH)
+, %(file_path)s
+#endif
+},"""
+
+template_metapath_loader_extension_module_entry = """\
+{%(module_name)s, NULL, 0, 0, %(flags)s
+#if defined(_NUITKA_FREEZER_HAS_FILE_PATH)
+, %(file_path)s
+#endif
+},"""
+
+template_metapath_loader_bytecode_module_entry = """\
+{%(module_name)s, NULL, %(bytecode)s, %(size)d, %(flags)s
+#if defined(_NUITKA_FREEZER_HAS_FILE_PATH)
+, %(file_path)s
+#endif
+},"""
+
+template_metapath_loader_excluded_module_entry = """\
+{%(module_name)s, NUITKA_CAST_INIT_REASON(%(exclusion_reason)s), 0, 0, %(flags)s
+#if defined(_NUITKA_FREEZER_HAS_FILE_PATH)
+, NULL
+#endif
+},"""
+
+
+template_metapath_loader_body = r"""
+/* Code to register embedded modules for meta path based loading if any. */
+
+#include "nuitka/prelude.h"
+
+/* Use a hex version of our own to compare for versions. We do not care about pre-releases */
+#if PY_MICRO_VERSION < 16
+#define PYTHON_VERSION (PY_MAJOR_VERSION * 256 + PY_MINOR_VERSION * 16 + PY_MICRO_VERSION)
+#else
+#define PYTHON_VERSION (PY_MAJOR_VERSION * 256 + PY_MINOR_VERSION * 16 + 15)
+#endif
+
+#include "nuitka/constants_blob.h"
+
+#include "nuitka/tracing.h"
+#include "nuitka/unfreezing.h"
+
+/* Type bool, spell-checker: ignore stdbool */
+#ifndef __cplusplus
+#include <stdbool.h>
+#endif
+
+#if %(bytecode_count)d > 0
+static unsigned char *bytecode_data[%(bytecode_count)d];
+#else
+static unsigned char **bytecode_data = NULL;
+#endif
+
+/* Helper for portable cast, to use string literals as module_init_func */
+#ifdef __cplusplus
+#define NUITKA_CAST_INIT_REASON(x) reinterpret_cast<module_init_func>((void*)(x))
+#else
+#define NUITKA_CAST_INIT_REASON(x) (module_init_func)(x)
+#endif
+
+/* Table for lookup to find compiled or bytecode modules included in this
+ * binary or module, or put along this binary as extension modules. We do
+ * our own loading for each of these.
+ */
+%(metapath_module_decls)s
+
+static struct Nuitka_MetaPathBasedLoaderEntry meta_path_loader_entries[] = {
+%(metapath_loader_inittab)s
+    {NULL, NULL, 0, 0, 0}
+};
+
+static void _loadBytesCodesBlob(PyThreadState *tstate) {
+    static bool init_done = false;
+
+    if (init_done == false) {
+        // Note needed for mere data.
+        loadConstantsBlob(tstate, (PyObject **)bytecode_data, ".bytecode");
+
+        init_done = true;
+    }
+}
+
+void setupMetaPathBasedLoader(PyThreadState *tstate) {
+    static bool init_done = false;
+    if (init_done == false) {
+        _loadBytesCodesBlob(tstate);
+        registerMetaPathBasedLoader(meta_path_loader_entries, bytecode_data);
+
+        init_done = true;
+    }
+}
+
+// This provides the frozen (compiled bytecode) files that are included if
+// any.
+
+// These modules should be loaded as bytecode. They may e.g. have to be loadable
+// during "Py_Initialize" already, or for irrelevance, they are only included
+// in this un-optimized form. These are not compiled by Nuitka, and therefore
+// are not accelerated at all, merely bundled with the binary or module, so
+// that CPython library can start out finding them.
+
+struct frozen_desc {
+    char const *name;
+    int index;
+    int size;
+};
+
+static struct frozen_desc _frozen_modules[] = {
+%(frozen_modules)s
+    {NULL, 0, 0}
+};
+
+
+void copyFrozenModulesTo(struct _frozen *destination) {
+    NUITKA_PRINT_TIMING("copyFrozenModulesTo(): Calling _loadBytesCodesBlob.");
+    _loadBytesCodesBlob(NULL);
+
+    NUITKA_PRINT_TIMING("copyFrozenModulesTo(): Updating frozen module table sizes.");
+
+    struct frozen_desc *current = _frozen_modules;
+
+    for (;;) {
+        destination->name = (char *)current->name;
+        destination->code = bytecode_data[current->index];
+        destination->size = current->size;
+#if PYTHON_VERSION >= 0x3b0
+        destination->is_package = current->size < 0;
+        destination->size = Py_ABS(destination->size);
+#if PYTHON_VERSION < 0x3d0
+        destination->get_code = NULL;
+#endif
+#endif
+        if (destination->name == NULL) break;
+
+        current += 1;
+        destination += 1;
+    };
+}
+
+#if _NUITKA_MODULE_MODE
+
+struct Nuitka_MetaPathBasedLoaderEntry const *getLoaderEntry(char const *name) {
+    struct Nuitka_MetaPathBasedLoaderEntry *current = meta_path_loader_entries;
+
+    while (current->name != NULL) {
+        if ((current->flags & NUITKA_TRANSLATED_FLAG) != 0) {
+            current->name = UN_TRANSLATE(current->name);
+            current->flags -= NUITKA_TRANSLATED_FLAG;
+        }
+
+        if (strcmp(name, current->name) == 0) {
+            return current;
+        }
+
+        current++;
+    }
+
+    assert(false);
+    return NULL;
+}
+#endif
+
+"""
+
+from . import TemplateDebugWrapper  # isort:skip
+
+TemplateDebugWrapper.checkDebug(globals())
+
+#     Part of "Nuitka", an optimizing Python compiler that is compatible and
+#     integrates with CPython, but also works on its own.
+#
+#     Licensed under the GNU Affero General Public License, Version 3 (the "License");
+#     you may not use this file except in compliance with the License.
+#     You may obtain a copy of the License at
+#
+#        http://www.gnu.org/licenses/agpl.txt
+#
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
