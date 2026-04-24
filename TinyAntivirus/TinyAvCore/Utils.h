d88ba67932dc3c4366add72789fb0e7b11ddfce3
Emirhan Uçan <130374789+Siradankullanici@users.noreply.github.com>
2026-04-24 15:02:00 +0300

    I will move to other repo then only take open source parts

--- TinyAntivirus/TinyAvCore/Utils.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <TinyAvBase.h>
-
-StringW AnsiToUnicode(__in StringA * str);
-StringW AnsiToUnicode(__in StringA& str);
-StringA UnicodeToAnsi(__in StringW * str);
-StringA UnicodeToAnsi(__in StringW& str);
