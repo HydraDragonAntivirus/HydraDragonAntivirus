a2d58b3c1185603b0c8d0be3cdddf75d86760b1c
Emirhan Uçan <semaemirhan555@gmail.com>
2026-04-29 13:52:22 +0300

    patch

--- OpenEDR/edrav2/iprj/libcore/inc/build_info.h
+++ OpenEDR/edrav2/iprj/libcore/inc/build_info.h
@@ -1,2 +1,2 @@
-#define CMD_BUILD_EXTRA "Commit: 971c0b4f, build time: 2026.04.29 02:40:37"
+#define CMD_BUILD_EXTRA "Commit: c01e64c8, build time: 2026.04.29 13:47:03"
 #define CMD_VERSION_BUILD 0 
