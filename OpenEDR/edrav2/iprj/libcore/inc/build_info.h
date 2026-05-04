ba83f81f4cfaccfe7e8c3689b1fe534f542a1326
Emirhan Uçan <semaemirhan555@gmail.com>
2026-05-05 00:12:58 +0300

    no upload only test

--- OpenEDR/edrav2/iprj/libcore/inc/build_info.h
+++ OpenEDR/edrav2/iprj/libcore/inc/build_info.h
@@ -1,2 +1,2 @@
-#define CMD_BUILD_EXTRA "Commit: af48571b, build time: 2026.04.29 19:16:17"
+#define CMD_BUILD_EXTRA "Commit: cd9f7ba8, build time: 2026.05.05 00:03:53"
 #define CMD_VERSION_BUILD 0 
