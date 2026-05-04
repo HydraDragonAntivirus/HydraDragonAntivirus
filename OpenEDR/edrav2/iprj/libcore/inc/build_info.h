1185bee6189cbef0d0030ed049f5cdb3b059fd21
Emirhan Uçan <semaemirhan555@gmail.com>
2026-05-04 23:17:54 +0300

    update yml and more

--- OpenEDR/edrav2/iprj/libcore/inc/build_info.h
+++ OpenEDR/edrav2/iprj/libcore/inc/build_info.h
@@ -1,2 +1,2 @@
-#define CMD_BUILD_EXTRA "Commit: af48571b, build time: 2026.04.29 19:16:17"
+#define CMD_BUILD_EXTRA "Commit: b9b31d84, build time: 2026.05.04 23:10:27"
 #define CMD_VERSION_BUILD 0 
