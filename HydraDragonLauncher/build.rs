26d5297724a2b59886a066281187bd097a34c4db
Emirhan Uçan <semaemirhan555@gmail.com>
2026-05-12 14:10:56 +0300

    manfiest

--- HydraDragonLauncher/build.rs
+++ HydraDragonLauncher/build.rs
@@ -2,6 +2,17 @@ fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("../hydradragon/assets/HydraDragonAV.ico");
+        res.set_manifest(r#"
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>
+"#);
         res.compile().unwrap();
     }
 }
