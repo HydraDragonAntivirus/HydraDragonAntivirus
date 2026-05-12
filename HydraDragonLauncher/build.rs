afcf6a5071a4bfc3d867f76e5f79c003c0e3bbae
Emirhan Uçan <semaemirhan555@gmail.com>
2026-05-12 14:26:59 +0300

    Fixing launcher

--- HydraDragonLauncher/build.rs
+++ HydraDragonLauncher/build.rs
@@ -2,7 +2,8 @@ fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("../hydradragon/assets/HydraDragonAV.ico");
-        res.set_manifest(r#"
+        res.set_manifest(
+            r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
   <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
     <security>
@@ -12,7 +13,8 @@ fn main() {
     </security>
   </trustInfo>
 </assembly>
-"#);
+"#,
+        );
         res.compile().unwrap();
     }
 }
