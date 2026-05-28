8d6ddefca10e32126880d5ca1e5686ad0dd18746
Emirhan Uçan <semaemirhan555@gmail.com>
2026-05-28 22:51:00 +0300

    Update compile_all.cmd

--- hydradragon/yara/compile_all.cmd
+++ hydradragon/yara/compile_all.cmd
@@ -10,7 +10,7 @@ call :CompileYarac machine_learning_pe.yar machine_learning_pe.yrc || exit /b %E
 call :CompileYarac machine_learning_js.yar machine_learning_js.yrc || exit /b %ERRORLEVEL%
 call :CompileYarac icewater.yar icewater.yrc || exit /b %ERRORLEVEL%
 call :CompileYaraX yaraxtr.yar yaraxtr.yrc || exit /b %ERRORLEVEL%
-call :CompileYarac WindowsDefender.yar WindowsDefender.yrc || exit /b %ERRORLEVEL%
+:: call :CompileYarac WindowsDefender.yar WindowsDefender.yrc || exit /b %ERRORLEVEL% :: Windows Defender rules string conditions might wrong also not open source.
 
 endlocal
 exit /b 0
