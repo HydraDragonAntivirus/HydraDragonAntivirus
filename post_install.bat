bc1622865cb8dc5444108956457431a717d975b6
Emirhan Uçan <semaemirhan555@gmail.com>
2026-05-29 23:58:17 +0300

    removing

--- post_install.bat
+++ post_install.bat
@@ -101,29 +101,23 @@ if errorlevel 1 (
 )
 
 :: --------------------------------------------------------
-:: 6) Install OwlyshieldRansomFilter driver
-:: --------------------------------------------------------
-call :install_driver_inf "OwlyshieldRansomFilter" "%HYDRADRAGON_DIR%\Owlyshield\OwlyshieldRansomFilter\OwlyshieldRansomFilter.inf" required
-if errorlevel 1 exit /b 1
-
-:: --------------------------------------------------------
-:: 7) Install MBRFilter driver
+:: 6) Install MBRFilter driver
 :: --------------------------------------------------------
 call :install_driver_inf "MBRFilter" "%HYDRADRAGON_DIR%\MBRFilter\MBRFilter.inf" required
 if errorlevel 1 exit /b 1
 
 :: --------------------------------------------------------
-:: 8) Install RedDbg driver (AMD Hypervisor)
+:: 7) Install RedDbg driver (AMD Hypervisor)
 :: --------------------------------------------------------
 call :install_driver_inf "RedDbg" "%HYDRADRAGON_DIR%\Owlyshield\RedDbg\RedDbgDrv.inf" optional
 
 :: --------------------------------------------------------
-:: 9) Install HyperDbg driver (Intel Hypervisor)
+:: 8) Install HyperDbg driver (Intel Hypervisor)
 :: --------------------------------------------------------
 call :install_driver_inf "HyperDbg" "%HYDRADRAGON_DIR%\Owlyshield\HyperDbg\hyperhv.inf" optional
 
 :: --------------------------------------------------------
-:: 10) Register HydraDragonAntivirus scheduled task (autostart after reboot)
+:: 9) Register HydraDragonAntivirus scheduled task (autostart after reboot)
 :: --------------------------------------------------------
 set "HD_TASK_EXE=%HYDRADRAGON_DIR%\HydraDragonController\hydradragoncontroller.exe"
 set "HD_TASK_EXISTS=0"
@@ -163,7 +157,7 @@ if not "%RUN_EXIT%"=="0" (
 :after_hd_task
 
 :: --------------------------------------------------------
-:: 11) Install OpenEDR service
+:: 10) Install OpenEDR service
 :: --------------------------------------------------------
 set "EDR_EXE=C:\Program Files\HydraDragonAntivirus\OpenEDR\edrsvc.exe"
 call :log [*] Checking for OpenEDR at "%EDR_EXE%"...
@@ -198,7 +192,7 @@ if not errorlevel 1 (
 :after_openedr
 
 :: --------------------------------------------------------
-:: 12) Disable Code Integrity for OpenEDR DLL injection
+:: 11) Disable Code Integrity for OpenEDR DLL injection
 :: --------------------------------------------------------
 call :log [*] Configuring system for OpenEDR DLL injection...
 call :log [*] Disabling Code Integrity checks to allow edrpm DLL injection...
@@ -236,7 +230,7 @@ call :log [*] These settings allow edrpm32.dll and edrpm64.dll to be injected in
 call :log [*] To re-enable security later, run: OpenEDR\edrav2\iprj\edrpm\enable_ci_back.bat
 
 :: --------------------------------------------------------
-:: 13) Cleanup and Restart
+:: 12) Cleanup and Restart
 :: --------------------------------------------------------
 echo [+] All installation steps complete!
 echo [*] Restarting system in 10 seconds to activate security drivers...
