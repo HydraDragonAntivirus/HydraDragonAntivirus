123b0ad0ce59f716c2f759e14fc8033e24f4ebe2
Emirhan Uçan <semaemirhan555@gmail.com>
2026-08-30 22:15:56 +0300

    Update controller.cpp

--- OpenEDR/edrav2/iprj/libsysmon/src/controller.cpp
+++ OpenEDR/edrav2/iprj/libsysmon/src/controller.cpp
@@ -616,9 +616,6 @@ bool SystemMonitorController::parseEvent(const Byte* pBuffer, const Size nBuffer
 				// (which overwrites "type" from baseType) emits LLE_DEVICE_IOCTL
 				// and Owlyshield dispatches them via the owlyHook sub-dict.
 				eEvent = Event::LLE_DEVICE_IOCTL;
-				LOGLVL(Debug, FMT("Parsed OwlyHook event: func=" << string::convertWCharToUtf8(vEvent.get("owlyHookFunctionName", L""))
-					<< " srcPid=" << vEvent.get("owlyHookSourcePid", uint32_t(0))
-					<< " tgtPid=" << vEvent.get("owlyHookTargetPid", uint32_t(0))));
 			}
 
 			// Build the owlyHv sub-dict from the nested owlyHv.* dict (hypervisor
